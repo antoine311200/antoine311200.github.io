ae18d4158d2d2b9151e5ed83bfa176a30b77a36b
antoine311200 <antoine311200@gmail.com>
2024-02-13 18:32:34 +0100

    Create a novel background with radial gradient rather than svg waves

--- src/App.js
+++ src/App.js
@@ -14,9 +14,17 @@ export default function App() {
   return (
     <Template>
       <div className="text-center pt-20" style={{ zIndex: 10 }}>
-        <h1 className="text-5xl py-2 text-white font-medium">Antoine Debouchage</h1>
-        <h4 className="text-2xl py-3 text-white">
-          <span className="">Machine Learning</span>, Quantum Computing, Mathematics</h4>
+        <h1 className="text-5xl py-2 text-gray-100 font-semibold">Antoine Debouchage</h1>
+        <h4 className="text-2xl py-3 text-gray-200">
+          <span className="">Machine Learning</span>, Quantum Computing, Mathematics
+        </h4>
+        <br />
+        <br />
+        {/* <div className='text-left w-1/2 mx-auto'>
+          <p className="text-lg text-gray-300">
+            <span className="font-bold">Welcome</span> to my personal website. My name is Antoine Debouchage, I am a Master student at CentraleSupélec & Ecole Normale Supérieure Paris-Saclay. I am passionate about <span className="font-bold">Machine Learning</span>, Quantum Computing and Mathematics.
+          </p>
+        </div> */}
       </div>
     </Template>
   );
--- src/components/template.js
+++ src/components/template.js
@@ -6,14 +6,10 @@ import '../App.css';
 
 export default function Template({ children, iconColor }) {
   return (
-    <div>
-      <div className='min-h-screen fixed top-0 left-0 w-screen h-screen bg-cover bg-no-repeat bg-center bg-slate-800'
-        style={{
-          backgroundImage: `url('/waves.svg')`,
-          zIndex: -1
-        }}
-      ></div>
-
+    <div className='min-h-screen text-gray-500' style={{
+      'background': '#0f172a',
+      'backgroundImage': 'radial-gradient(circle at 10% 90%, #203157, transparent 500px), radial-gradient(circle at 95% 20%, #192745, transparent 500px)',
+    }}>
       <Header />
 
       {children}
