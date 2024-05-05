13306dc76447a1a28d2c8e94ce56ff23b11045b7
antoine311200 <antoine311200@gmail.com>
2024-05-05 16:48:46 +0200

    Update contact page for mobile

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ import { FaGithub, FaTwitter, FaLinkedin, FaKaggle, FaEnvelope } from 'react-ico
 
 const LinkComponent = ({ href, text, icon }) => {
   return (
-    <div className="skeleton-black flex items-left space-x-4">
+    <div className="flex items-left space-x-4">
       {icon}
       <a
         className="text-gray-800 text-left"
@@ -30,22 +30,16 @@ export default function App() {
 
   return (
     <Template>
-      <div className="skeleton flex flex-col justify-center mx-4 md:mx-10 mt-10 text-center bg-white rounded-lg p-4 md:p-10">
-      {/* <div> */}
+      <div className="flex flex-col justify-center mx-4 md:mx-10 mt-10 text-center bg-white rounded-lg p-4 md:p-10">
         <h1 className="text-3xl font-bold text-gray-800 mb-12">Contact Me</h1>
 
-        <div className="skeleton-black justify-center flex flex-col md:flex-row items-center space-x-12">
-          <div className="skeleton-black flex flex-col items-left space-y-4 text-sm md:text-base">
+        <div className="flex flex-col items-center justify-center md:flex-row space-x-12">
+          <div className="flex flex-col items-left space-y-4 text-sm md:text-base">
             <LinkComponent href="https://github.com/antoine311200" text="Github @antoine311200" icon={<FaGithub size={24} className='text-grey-500' />} />
             <LinkComponent href="https://twitter.com/antoine311200" text="Twitter @antoine311200" icon={<FaTwitter size={24} className='text-grey-500' />} />
             <LinkComponent href="https://www.kaggle.com/antoinedebouchage" text="Kaggle @antoinedebouchage" icon={<FaKaggle size={24} className='text-grey-500' />} />
             <LinkComponent href="https://www.linkedin.com/in/antoine-debouchage/" text="LinkedIn Antoine Debouchage" icon={<FaLinkedin size={24} className='text-grey-500' />} />
-          </div>
-
-          <div className="h-10"></div>
-
-          {/* Center */}
-          <div className="skeleton-black flex flex-col items-left space-y-4 text-sm md:text-base">
+            <div className="h-10"></div>
             <LinkComponent href="mailto:antoine311200@gmail.com" text="antoine311200@gmail.com" icon={<FaEnvelope size={24} className='text-grey-500' />} />
             <LinkComponent href="mailto:antoine.debouchage@student-cs.fr" text="antoine.debouchage@student-cs.fr" icon={<FaEnvelope size={24} className='text-grey-500' />} />
             <LinkComponent href="mailto:antoine.debouchage@ens-paris-saclay.fr" text="antoine.debouchage@ens-paris-saclay.fr" icon={<FaEnvelope size={24} className='text-grey-500' />} />
